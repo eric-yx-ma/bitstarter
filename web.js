410d3678ebe19a5df628076eb54d55e1e03cc2c6
Eric Ma <eric.yx.ma@gmail.com>
2013-07-22 16:56:48 +0000

    Changed web.js to handle index.html through buffer

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var buffer = new Buffer(fs.readFileSync('index.html', 'encoding'));
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
